3959d8a2d95bec1ba663b66ce969ab5b9ce7c901
akiyoko <akiyoko@users.noreply.github.com>
2019-08-04 00:11:31 +0900

    api_view -> api_views

--- config/urls.py
+++ config/urls.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from api import views as api_view
-from apiv1 import views as apiv1_view
-from apiv2 import views as apiv2_view
+from api import views as api_views
+from apiv1 import views as apiv1_views
+from apiv2 import views as apiv2_views
 
 router = routers.DefaultRouter()
-router.register('books', apiv2_view.BookViewSet)
+router.register('books', apiv2_views.BookViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/books/', api_view.BookListCreateAPIView.as_view()),
-    path('api/books/<pk>/', api_view.BookRetrieveUpdateDestroyAPIView.as_view()),
-    path('api/v1/books/', apiv1_view.BookListCreateAPIView.as_view()),
-    path('api/v1/books/<pk>/', apiv1_view.BookRetrieveUpdateDestroyAPIView.as_view()),
+    path('api/books/', api_views.BookListCreateAPIView.as_view()),
+    path('api/books/<pk>/', api_views.BookRetrieveUpdateDestroyAPIView.as_view()),
+    path('api/v1/books/', apiv1_views.BookListCreateAPIView.as_view()),
+    path('api/v1/books/<pk>/', apiv1_views.BookRetrieveUpdateDestroyAPIView.as_view()),
     path('api/v2/', include(router.urls)),
 ]
