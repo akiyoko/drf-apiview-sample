121c369899a2f616328d4db0a0e42cf5f70df7f8
akiyoko <akiyoko@users.noreply.github.com>
2020-12-13 09:31:55 +0900

    api -> apiv0

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
 
-from api import views
 from apitest import views as apitest_views
+from apiv0 import views as apiv0_views
 from apiv1 import views as apiv1_views
 from apiv2 import views as apiv2_views
 
@@ -15,8 +15,8 @@ router.register('books', apiv2_views.BookViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/books/', views.BookListCreateAPIView.as_view()),
-    path('api/books/<pk>/', views.BookRetrieveUpdateDestroyAPIView.as_view()),
+    path('api/v0/books/', apiv0_views.BookListCreateAPIView.as_view()),
+    path('api/v0/books/<pk>/', apiv0_views.BookRetrieveUpdateDestroyAPIView.as_view()),
     path('api/v1/books/', apiv1_views.BookListCreateAPIView.as_view()),
     path('api/v1/books/<pk>/', apiv1_views.BookRetrieveUpdateDestroyAPIView.as_view()),
     path('api/v2/', include(router.urls)),
